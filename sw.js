ebbd099daacc2a80bfed7b895af0717449e22e2f
uyadifjall <uyadi.fjall@gmail.com>
2026-05-30 21:46:08 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'kondate-v1';
+const STATIC_ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // ネットワーク優先、失敗時キャッシュ
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
